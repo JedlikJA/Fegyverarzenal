4403f800e7df5651df669df951df599889387e47
juhász.ábel.kristóf <juhasz.abel99@gmail.com>
2020-02-25 13:17:04 +0100

    fegyvertár lista

--- Fegyverarzenal/Program.cs
+++ Fegyverarzenal/Program.cs
@@ -8,9 +8,17 @@ namespace Fegyverarzenal
 {
     class Program
     {
+        enum fegyverek {gránát , uzi , m4 , ak47};
         static void Main(string[] args)
         {
-            
+            fegyverek kezemben = fegyverek.m4;
+            List<fegyverek> fegyvertár = new List<fegyverek>();
+            fegyvertár.Add(fegyverek.uzi);
+            fegyvertár.Add(fegyverek.ak47);
+            foreach (fegyverek item in fegyvertár)
+            {
+                Console.WriteLine(item);
+            }
             Console.ReadKey();
         }
     }
